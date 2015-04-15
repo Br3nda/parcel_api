d5db66461bc243f99e85702a7ac1d17a7dc58acf
mukul <mugoyal@enbake.com>
2015-04-15 21:13:17 +0530

    Added gem dependency for test framework, Authentication and HttpClient #1

---
+++ spec/vcr_setup.rb
@@ -0,0 +1,9 @@
+require 'vcr'
+
+VCR.configure do |config|
+  config.cassette_library_dir     = 'spec/fixtures/vcr_cassettes'
+  config.hook_into                  :webmock
+  config.ignore_localhost         = true
+  config.default_cassette_options = { :record => :new_episodes }
+  config.configure_rspec_metadata!
+end
