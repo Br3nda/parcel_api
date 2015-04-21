d6bc282f6074c1b2c02c6e8900b3a885a9f7b19f
mukul <mugoyal@enbake.com>
2015-04-21 19:44:10 +0530

    Raise exception in faraday connection and created mock.rb #2

--- lib/parcel_api/address.rb
+++ lib/parcel_api/address.rb
@@ -7,16 +7,17 @@ module ParcelApi
 
     AUTOCOMPLETE_URL  = '/v2/addresses'
 
-    def initialize
-      @connection = ParcelApi::Client.create
+    def initialize(params={})
+      address = params.fetch(:address, 'http://addressing-nzpg.au.cloudhub.io')
+      @connection = ParcelApi::Client.create(client_id: params[:client_id], client_secret: params[:client_secret], username: params[:username], password: params[:password], address: address)
     end
 
     def search(query,count=10)
       collectedAddresses = []
       count = 10 if count > 10
       options = {
-        :q => query,
-        :count => count
+        q: query,
+        count: count
       }
       response = @connection.get AUTOCOMPLETE_URL, options
       dataResults = response.body['addresses']
@@ -31,8 +32,8 @@ module ParcelApi
       details_url = File.join(AUTOCOMPLETE_URL, address_id.to_s)
       response = @connection.get details_url
       complete_address_details = response.body['address']
-      data = OpenStruct.new(complete_address_details)
-      return data
+      return OpenStruct.new(complete_address_details)
+    end
     end
 
   end
--- lib/parcel_api/client.rb
+++ lib/parcel_api/client.rb
@@ -3,16 +3,20 @@ require 'faraday_middleware'
 
 module ParcelApi
   class Client
-    def self.create(options = {})
-      connection = Faraday.new(url: 'http://addressinguat-nzpg.au.cloudhub.io') do |conn|
+    def self.create(options={})
+      if !options[:client_id] || !options[:client_secret] || !options[:username] || !options[:password]
+        raise ArgumentError, "Missing required client identifier."
+      end
+      connection = Faraday.new(url: options[:address]) do |conn|
         conn.request :oauth2,
-        client_id: ENV['CLIENT_ID'] ,
-        client_secret: ENV['CLIENT_SECRET'],
-        username: ENV['USER_NAME'],
-        password: ENV['PASSWORD'],
+        client_id: options[:client_id],
+        client_secret: options[:client_secret],
+        username: options[:username],
+        password: options[:password],
         grant_type: 'password'
         conn.request :json
         conn.response :json
+        conn.use Faraday::Response::RaiseError   # raise exceptions on 40x, 50x responses
         conn.adapter Faraday.default_adapter
       end
     end
