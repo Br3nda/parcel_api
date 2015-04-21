b1e684c258335f0e3e3bc86ec2bf10db926cedd6
mukul <mugoyal@enbake.com>
2015-04-21 10:39:59 +0530

    Return full address details and array of Ostruct objects on search #2

--- lib/parcel_api/address.rb
+++ lib/parcel_api/address.rb
@@ -7,28 +7,32 @@ module ParcelApi
 
     AUTOCOMPLETE_URL  = '/v2/addresses'
 
-    def search(query,count)
+    def initialize
+      @connection = ParcelApi::Client.create
+    end
+
+    def search(query,count=10)
       collectedAddresses = []
+      count = 10 if count > 10
       options = {
         :q => query,
         :count => count
       }
-      @connection = ParcelApi::Client.create
       response = @connection.get AUTOCOMPLETE_URL, options
       dataResults = response.body['addresses']
       dataResults.each do |address|
-        complete_address_details = address_detail(address)
-        data = OpenStruct.new(complete_address_details)
+        data = OpenStruct.new(address_id: address['address_id'], full_address: address['full_address'])
         collectedAddresses << data
       end
       return collectedAddresses 
     end
 
-    def address_detail(address)
-      details_url = File.join(AUTOCOMPLETE_URL, address['address_id'].to_s)
+    def address_detail(address_id)
+      details_url = File.join(AUTOCOMPLETE_URL, address_id.to_s)
       response = @connection.get details_url
       complete_address_details = response.body['address']
-      return complete_address_details
+      data = OpenStruct.new(complete_address_details)
+      return data
     end
 
   end
