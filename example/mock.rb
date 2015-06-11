4256b7820b666d872c3bd398cd8c49b4b14f0006
Robert Coleman <github@robert.net.nz>
2015-06-11 17:40:09 +1200

    tidy up ParcelApi::Client

--- example/mock.rb
+++ example/mock.rb
@@ -1,17 +1,19 @@
-module Example
-  class Mock
-    # ParcelAddress API is a GET API that contains methods that return valid matching domestic or international addresses.
-    #You need to configure credentials to make API calls. You can pass configurations options in ParcelApi::Address.new()
+require 'parcel_api'
 
-    client = ParcelApi::Address.new(client_id: ENV['CLIENT_ID'], client_secret: ENV['CLIENT_SECRET'], username: ENV['USER_NAME'], password: ENV['PASSWORD'])
-
-    #Search Domestic Address for NZ
-
-    client.search('151 vic', 10)
-
-    #Use address_detail method to retrieve a complete set of the address detail. In this method you have to pass the address_id 
-
-    client.address_detail('325595')
-
-  end
+# configure the client
+client = ParcelApi::Client.new.tap do |config|
+  config.client_id     = ENV['CLIENT_ID']
+  config.client_secret = ENV['CLIENT_SECRET']
+  config.username      = ENV['USERNAME']
+  config.password      = ENV['PASSWORD']
 end
+
+# address = ParcelApi::Address.new
+#
+# #Search Domestic Address for NZ
+#
+# address.search('151 vic', 10)
+#
+# #Use address_detail method to retrieve a complete set of the address detail. In this method you have to pass the address_id
+#
+# address.address_detail('325595')
--- lib/parcel_api/client.rb
+++ lib/parcel_api/client.rb
@@ -3,40 +3,52 @@ require 'faraday_middleware'
 
 module ParcelApi
   class Client
-    def self.create(options={})
-      if !options[:client_id] || !options[:client_secret] || !options[:username] || !options[:password]
-        raise ParcelApi::Error, "Missing required client identifier."
-      end
-      token = ParcelApi::Client.get_token(client_id: options[:client_id], client_secret: options[:client_secret],
-                username: options[:username], password: options[:password])
 
-      connection = Faraday.new(url: options[:address]) do |conn|
-        conn.authorization :Bearer, token
-        conn.headers['client_id'] = options[:client_id]
-        conn.request :json
+    attr_accessor :client_id,
+      :client_secret,
+      :username,
+      :password,
+      :address
+
+    def initialize
+      @client_id = client_id || ENV['CLIENT_ID']
+      @client_secret = client_secret || ENV['CLIENT_SECRET']
+      @username = username || ENV['USERNAME']
+      @password = password || ENV['PASSWORD']
+    end
+
+    def create
+      Faraday.new(url: address) do |conn|
+        conn.authorization :bearer, token
+        conn.headers['client_id'] = @client_id
+        conn.request  :json
         conn.response :json
-        conn.use ParcelApi::ResponseError      # raise exceptions on 40x, 50x responses
-        conn.adapter Faraday.default_adapter
+        conn.use      ParcelApi::ResponseError
+        conn.adapter  Faraday.default_adapter
       end
     end
 
     private
 
-    def self.get_token(options={})
-      connection = Faraday.new({ ssl: { verify: false } }) do |conn|
-        conn.request :url_encoded
-        conn.response :json
-        conn.use ParcelApi::ResponseError
-        conn.adapter Faraday.default_adapter
+    def token
+      @token ||= begin
+        params = {
+          client_id:     @client_id,
+          client_secret: @client_secret,
+          username:      @username,
+          password:      @password,
+          grant_type:    'password',
+        }
+
+        connection = Faraday.new do |conn|
+          conn.request  :url_encoded
+          conn.response :json
+          conn.use      ParcelApi::ResponseError
+          conn.adapter  Faraday.default_adapter
+        end
+        response = connection.post 'https://oauth.nzpost.co.nz/as/token.oauth2', params
+        response.body['access_token']
       end
-      params = {'client_id' => options[:client_id],
-                'client_secret' => options[:client_secret],
-                'username' => options[:username],
-                'password' => options[:password],
-                'grant_type' => 'password'
-               }
-      response = connection.post 'https://oauth.nzpost.co.nz/as/token.oauth2', params
-      return response.body['access_token']
     end
 
   end
