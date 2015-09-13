2fbaad0454a82800cfd0d6f257466650881f82ac
Robert Coleman <github@robert.net.nz>
2015-09-13 16:57:01 +1200

    fix codeclimate

--- spec/vcr_setup.rb
+++ spec/vcr_setup.rb
@@ -1,8 +1,9 @@
 require 'vcr'
 
 VCR.configure do |config|
-  config.cassette_library_dir     = 'spec/fixtures/vcr_cassettes'
-  config.hook_into                  :webmock
-  config.ignore_localhost         = true
+  config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
+  config.hook_into :webmock
+  config.ignore_localhost = true
   config.configure_rspec_metadata!
+  config.ignore_hosts 'codeclimate.com'
 end
